1e6f81c8ea072f71930be5d9a4861ed03797ad11
Christian Todd <christiantodd27@icloud.com>
2017-09-12 10:46:08 -0400

    link up.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 import './index.scss';
 
-const d = document;
-const body = document.body;
-const state = {
+const portfolioAppState = {
   slider0: 0,
   slider1: 0,
   slider2: 0
 };
+
 /* $document.ready */
-d.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', event => {
+  attachSliderHandlers();
   /* grab pertinent elements */
-  const about = d.getElementById('about');
-  const port = d.getElementById('portfolio');
-  const contact = d.getElementById('contact');
-  const nav = d.getElementById('nav-id');
+  const about = document.getElementById('about');
+  const port = document.getElementById('portfolio');
+  const contact = document.getElementById('contact');
+  const nav = document.getElementById('nav-id');
 
   /* Attach onClick handlers to each link for scrollTo feature */
-  d.getElementById('about-link').onclick = e => {
+  document.getElementById('about-link').onclick = e => {
     e.preventDefault();
-    scrollTo(d.body, about.offsetTop, 450);
+    scrollTo(document.body, about.offsetTop, 450);
   };
-  d.getElementById('portfolio-link').onclick = e => {
+  document.getElementById('portfolio-link').onclick = e => {
     e.preventDefault();
-    scrollTo(d.body, port.offsetTop - 50, 450);
+    scrollTo(document.body, port.offsetTop - 50, 450);
   };
-  d.getElementById('contact-link').onclick = e => {
+  document.getElementById('contact-link').onclick = e => {
     e.preventDefault();
-    scrollTo(d.body, contact.offsetTop, 450);
+    scrollTo(document.body, contact.offsetTop, 450);
   };
 
   /* check for when to show navigation */
@@ -42,6 +42,7 @@ d.addEventListener('DOMContentLoaded', event => {
       : (nav.className = 'nav-container');
   };
 });
+
 /* Utility scroll to function */
 const scrollTo = (element: HTMLElement, to: number, duration: number) => {
   const start = element.scrollTop;
@@ -68,65 +69,66 @@ const easeInOut = (currentTime, start, change, duration) => {
   }
   currentTime -= 1;
   return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
-  const attachSliderHandlers = (): void => {
-    const sliders = Array.from(d.getElementsByClassName('slider'));
+};
 
-    sliders.forEach(slider => {
-      const buttons = slider.getElementsByTagName('button');
+/* Renders the sliders into the DOM */
+const attachSliderHandlers = (): void => {
+  const sliders = Array.from(document.getElementsByClassName('slider'));
 
-      Array.from(buttons).forEach((button, i) => {
-        i === 0
-          ? button.addEventListener('click', e => {
-              if (e.srcElement.tagName !== 'BUTTON') {
-                return;
-              } else {
-                const buttonId = e.target.id;
-                const stateKey = `slider${Number(buttonId.match(/\d/)[0])}`;
+  sliders.forEach(slider => {
+    const buttons = slider.getElementsByTagName('button');
 
-                prevThumb(button.id, state[stateKey]);
-              }
-            })
-          : button.addEventListener('click', e => {
-              if (e.srcElement.tagName !== 'BUTTON') {
-                return;
-              } else {
-                const buttonId = e.target.id;
-                const stateKey = `slider${Number(buttonId.match(/\d/)[0])}`;
+    Array.from(buttons).forEach((button, i) => {
+      i === 0
+        ? button.addEventListener('click', e => {
+            if (e.srcElement.tagName !== 'BUTTON') {
+              return;
+            } else {
+              const buttonId = e.target.id;
+              const stateKey = `slider${Number(buttonId.match(/\d/)[0])}`;
 
-                nextThumb(button.id, state[stateKey]);
-              }
-            });
-      });
+              prevThumb(button.id, portfolioAppState[stateKey]);
+            }
+          })
+        : button.addEventListener('click', e => {
+            if (e.srcElement.tagName !== 'BUTTON') {
+              return;
+            } else {
+              const buttonId = e.target.id;
+              const stateKey = `slider${Number(buttonId.match(/\d/)[0])}`;
+
+              nextThumb(button.id, portfolioAppState[stateKey]);
+            }
+          });
     });
+  });
 
-    /* animates the image */
-    const setTransform = (el: HTMLElement, nextIndex: number) => {
-      el.style['transform'] = `translate3d(${-(
-        nextIndex * el.offsetWidth
-      )}px, 0 , 0)`;
-    };
-
-    /* Increment current slider's index of visible picture and call to setTransform animation */
-    const nextThumb = (buttonId: string, currentIndex: number): void => {
-      const btnIndex = buttonId.match(/\d/)[0];
-      const nextIndex = currentIndex + 1;
-      const pos = Math.min(nextIndex, 2);
-      state[`slider${btnIndex}`] = pos;
-
-      const list = d.getElementById(`imgThumbList${btnIndex}`);
-      setTransform(list, pos);
-    };
-
-    /* Decrement current slider's index of visible picture and call to setTransform animation */
-    const prevThumb = (buttonId: string, currentIndex: number): void => {
-      const btnIndex = buttonId.match(/\d/)[0];
-      const nextIndex = currentIndex - 1;
-      const pos = Math.max(nextIndex, 0);
-      state[`slider${btnIndex}`] = pos;
-
-      const list = d.getElementById(`imgThumbList${btnIndex}`);
-      setTransform(list, pos);
-    };
+  /* animates the image */
+  const setTransform = (el: HTMLElement, nextIndex: number) => {
+    el.style['transform'] = `translate3d(${-(
+      nextIndex * el.offsetWidth
+    )}px, 0 , 0)`;
   };
-  attachSliderHandlers();
+
+  /* Increment current slider's index of visible picture and call to setTransform animation */
+  const nextThumb = (buttonId: string, currentIndex: number): void => {
+    const btnIndex = buttonId.match(/\d/)[0];
+    const nextIndex = currentIndex + 1;
+    const pos = Math.min(nextIndex, 2);
+    portfolioAppState[`slider${btnIndex}`] = pos;
+
+    const list = document.getElementById(`imgThumbList${btnIndex}`);
+    setTransform(list, pos);
+  };
+
+  /* Decrement current slider's index of visible picture and call to setTransform animation */
+  const prevThumb = (buttonId: string, currentIndex: number): void => {
+    const btnIndex = buttonId.match(/\d/)[0];
+    const nextIndex = currentIndex - 1;
+    const pos = Math.max(nextIndex, 0);
+    portfolioAppState[`slider${btnIndex}`] = pos;
+
+    const list = document.getElementById(`imgThumbList${btnIndex}`);
+    setTransform(list, pos);
+  };
 };
